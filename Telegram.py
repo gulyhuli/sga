8cf0a13170a181a218768f6a6c2ea95e1ca1c82a
wunted <wunted@yandex.ru>
2016-12-24 12:48:55 +0300

    Delete

--- Telegram.py
+++ Telegram.py
@@ -186,11 +186,13 @@ def hanlde_text(message):
         reg = 0
     elif message.text == "Смотреть картинки":
         reg = 2
-        bot.send_message(str(message.from_user.id), "Что ты хочешь посмотреть?")
+        bot.send_message(str(message.from_user.id),
+                         "Что ты хочешь посмотреть? После окончания просмотра, нажмите \Закончить")
 
     elif message.text == "Смотреть видео":
         reg = 3
-        bot.send_message(str(message.from_user.id), "Что ты хочешь посмотреть?")
+        bot.send_message(str(message.from_user.id),
+                         "Что ты хочешь посмотреть? После окончания просмотра, нажмите \Закончить")
     elif message.text == "Курс валют" or message.text == "курс валют":
         reg = 0
         bot.send_message(str(message.from_user.id), "Информация от ЦБ РФ\n" +
